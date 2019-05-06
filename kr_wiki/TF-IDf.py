abf770770a84a2d37cf262f1510893a2ac69b973
JAEHUN-AN <dybe15@gmail.com>
2019-05-06 16:21:04 +0900

    Update TF-IDf.py

--- kr_wiki/TF-IDf.py
+++ kr_wiki/TF-IDf.py
@@ -1,9 +1,22 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
-corpus = [
-    'you know I want your love',
-    'I like you',
-    'what should I do ',
-]
+from sklearn.feature_extraction.text import CountVectorizer
+
+from numpy import dot
+from numpy.linalg import norm
+import numpy as np
+from konlpy.tag import Komoran
+komoran = Komoran()
+print(komoran.morphs(u'임진왜란 시기에 거북선을 만든 조선의 장군은?'))
+def cos_sim(A, B):
+       return dot(A, B)/(norm(A)*norm(B))
+
+
+'''
+vector = CountVectorizer()
+docs1 = vector.fit_transform(corpus).toarray()
+docs2 = vector.fit_transform(corpus2).toarray()
+
 tfidfv = TfidfVectorizer().fit(corpus)
 print(tfidfv.transform(corpus).toarray())
-print(tfidfv.vocabulary_)
+print(cos_sim(docs1, docs2))
+'''

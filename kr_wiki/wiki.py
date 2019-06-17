43f49f8bd05679ba1ff5c9bc70ba09f425826491
hlee118 <gustlr2510@gmail.com>
2019-06-17 13:27:51 +0900

    위키 수정

--- kr_wiki/wiki.py
+++ kr_wiki/wiki.py
@@ -2,20 +2,10 @@ import sys
 import wikipediaapi
 from sklearn.feature_extraction.text import TfidfVectorizer
 wiki_ko = wikipediaapi.Wikipedia('ko')
-# # 입력된 단어들
-query = input();
+
+## 입력된 단어들
+query = sys.argv[1];
 page_py = wiki_ko.page(query)
 q1 = page_py.summary[0:] #문서 검색 내용
-print(q1)
-
-corpus = [
-    'you know I want your love',
-    'I like you',
-    'what should I do ',
-]
-tfidfv = TfidfVectorizer().fit(corpus)
-# # 입력된 단어들
-query = input();
-#
-# # 출력 대답
-print("결과가 없습니다", 100)
+print(q1.split('\n')[0])
+print(1)
